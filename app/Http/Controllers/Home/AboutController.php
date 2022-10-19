b1bd8b2049e5d32935ce404c26675b14218d637b
Mim <farhad15-11090@diu.edu.bd>
2022-10-19 20:50:19 +0600

    bug

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -24,7 +24,7 @@ class AboutController extends Controller
             $image = $request->file('about_image');
             $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();  // 3434343443.jpg
 
-            Image::make($image)->resize(523,605)->save('upload/home_about/'.$name_gen);
+            'Image'::make($image)->resize(523,605)->save('upload/home_about/'.$name_gen);
             $save_url = 'upload/home_about/'.$name_gen;
 
             About::findOrFail($about_id)->update([
@@ -60,5 +60,5 @@ class AboutController extends Controller
 
         } // end Else
 
-     } // End Method 
+     } // End Method
 }
